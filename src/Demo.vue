25d7d44fa508d4b2807d45ffbc29b4dcc6906cb4
yinlong22 <342241857@.com>
2020-04-19 15:00:09 +0800

    create

---
+++ src/Demo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="red">
+        demo11111
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    .red{
+        color: red;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,62 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
+
+const Vue = window.Vue
 
 Vue.config.productionTip = false
+import Demo from './Demo.vue'//优先使用demo-->模块化
 
+Vue.component('Demo2', {
+    template: `
+    <div>demo22222<div>
+   `
+})
+//Vue实例或Vue对象
 new Vue({
-  render: h => h(App),
+    components: {
+        Demo,//Demo:Demo的缩写
+        Demo3: {
+            template: `
+                <div>demo33333</div>
+            `
+        }
+    },
+
+    //生命周期
+    create(){
+        console.log('我在内存中，还没出现在页面中')
+    },mounted() {
+        console.log('我已出现在页面中')
+    },updated(){
+        console.log('更新了')
+        console.log(this.n)
+    },destroyed(){},
+
+    data() {
+        return {
+            n: 0,
+            array: [1, 2, 3, 4, 5, 6, 7, 8]
+        }
+    },
+
+    template: `
+        <div class="red">
+            {{n}}
+            <button @click="add">+1</button>
+            <Demo/>
+            <Demo2/>
+            <Demo3/>
+            <hr>
+            {{filter()}}
+        </div>
+    `,
+
+    methods: {
+        add() {
+            this.n += 1
+        },
+        filter() {
+            return this.array.filter(i => i % 2 === 0)
+        }
+    }
 }).$mount('#app')
